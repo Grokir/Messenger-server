45d7b3e1c78058408f2cee384d879f9259811e41
Grokir <132129213+Grokir@users.noreply.github.com>
2023-11-08 23:49:34 +0300

    files with Client data

---
+++ Client.java
@@ -0,0 +1,34 @@
+public class Client {
+    private  String m_login = "";
+    private String m_password = "";
+
+    private Person m_person_data;
+
+
+    public Client(
+            String login,
+            String password,
+            Person person_data
+    )
+    {
+        m_login = login;
+        m_password = password;
+        m_person_data = new Person(person_data);
+    }
+
+    // TODO: деструктор
+
+    public String getLogin(){
+        return m_login;
+    }
+    public String getPassword(){
+        return m_password;
+    }
+    public String getPersonFullName(){
+        return m_person_data.getFullName();
+    }
+    public String getPersonBirthday(){
+        return m_person_data.getDateOfBirth();
+    }
+
+}
---
+++ Person.java
@@ -0,0 +1,78 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Person {
+
+    private String m_name;
+
+    private String m_surname;
+
+    private String m_patronymic;
+
+    private Date m_birthday;
+
+    private int YEAR_FORMAT = 1900;
+    private int MONTH_FORMAT = 1;
+
+    private SimpleDateFormat format_date= new SimpleDateFormat("dd.MM.yyyy");
+
+    public Person(){
+        String m_name = "";
+        String m_surname = "";
+        String m_patronymic = "";
+        m_birthday = new Date();
+        m_birthday.setYear(0);
+        m_birthday.setMonth(0);
+        m_birthday.setDate(1);
+    }
+    public Person(
+            String surname,
+            String name,
+            String patronymic,
+            int birth_day,
+            int birth_month,
+            int birth_year
+    )
+    {
+        m_name = name;
+        m_surname = surname;
+        m_patronymic = patronymic;
+        m_birthday = new Date();
+
+        // в годах идёт прибавка к 1900-му году
+        // нумерация месяцев начинается с 0
+        m_birthday.setYear(birth_year - YEAR_FORMAT);
+        m_birthday.setMonth(birth_month - MONTH_FORMAT);
+        m_birthday.setDate(birth_day);
+    }
+    public Person(Person p){
+        this.m_name = p.m_name;
+        this.m_surname = p.m_surname;
+        this.m_patronymic = p.m_patronymic;
+        this.m_birthday = p.m_birthday;
+    }
+    public String getName(){
+        return m_name;
+    }
+    public String getSurname(){
+        return m_surname;
+    }
+    public String getPatronymic(){
+        return m_patronymic;
+    }
+    public String getDateOfBirth(){
+        return format_date.format(m_birthday);
+    }
+    public String forPrint(){
+        return m_surname + ' ' +
+                m_name + ' ' +
+                m_patronymic + '\n' +
+                format_date.format(m_birthday);
+    }
+    public String getFullName(){
+        return  m_surname + ' ' +
+                m_name + ' ' +
+                m_patronymic;
+    }
+    }
+}
