1984cab47e20e3e8b491fd9e372e200f0545dbb1
Grokir <132129213+Grokir@users.noreply.github.com>
2023-11-22 23:30:53 +0300

    Add files via upload

--- Backend/Message.java
+++ Backend/Message.java
@@ -8,13 +8,14 @@ public class Message {
     private SimpleDateFormat format_date= new SimpleDateFormat("dd.MM.yyyy");
     private SimpleDateFormat format_time = new SimpleDateFormat("HH:mm:ss");
     private Date m_date_dispatch;
-
     private String m_id_msg;
+
+    
     public Message(){
         m_from_whom = "";
         m_to_whom = "";
         m_msg_text = "";
-        m_id_msg = ""
+        m_id_msg = "";
         m_date_dispatch = new Date();
         m_date_dispatch.setYear(0);
         m_date_dispatch.setMonth(0);
